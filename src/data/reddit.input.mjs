f018eb1bbd953fb48cbd7498b23c6970aea0bdf9
marcinaj <andrew.marciniak@gmail.com>
2021-01-31 18:17:57 -0500

    chnage io exports to static classes

--- src/data/reddit.input.mjs
+++ src/data/reddit.input.mjs
@@ -1,39 +1,33 @@
 import reddit from './reddit.client.mjs';
 import ignore from './config.ignore.mjs';
 
-let _last_seen_id   = 0;
-let _last_poll_time = 0;
-let _poll_interval  = 3600;
-let _poll_cnt       = 10;
-let _cache          = [];
+let LAST_SEEN_ID   = 0;
+let LAST_POLL_TIME = 0;
+let POLL_INTERVAL  = 3600;
+let POLL_COUNT     = 10;
+let CACHE          = [];
 
 let _rx_has_space = new RegExp( '^.* .*$' );
 let _rx_ignore    = new RegExp( ignore.join( '|' ), 'i' );
-
-function init()
-{
-  reddit.init();
-  return !!reddit.client;
-}
 
 async function fetch()
 {
   if( !reddit.client )
     return false;
     
-  let id      = _last_seen_id;  
+  let id      = LAST_SEEN_ID;  
   let cnt_all = 0;
   let cnt_ok  = 0;
   let cnt_unq = 0;
   let posts   = new Map();
   
-  const test = ( post )=> ((id = parseInt( post.id, 36 )) > _last_seen_id
+  const test = ( post )=> ((id = parseInt( post.id, 36 )) > LAST_SEEN_ID
     && !!post.title.length 
     && _rx_has_space.test( post.title )
     && !_rx_ignore.test( post.title )
   );
   const put  = post => { ++cnt_all; if( test( post ) ) ++cnt_ok, posts.set( post.id, post.title ); };
-  const opts = { time: 'all', limit: _poll_cnt };
+  const opts = { time: 'all', limit: POLL_COUNT };
   
   let subreddit    = await reddit.client.getSubreddit( 'SatoshiStreetBets' );
     
@@ -41,38 +35,44 @@ async function fetch()
   (await subreddit.getTop( opts )).forEach( post => put( post ) );
   (await subreddit.getNew( opts )).forEach( post => put( post ) );
   
-  posts.forEach( text => (++cnt_unq, _cache.push( text )) );
-  _last_seen_id = id;
+  posts.forEach( text => (++cnt_unq, CACHE.push( text )) );
+  LAST_SEEN_ID = id;
   
   console.log( `reddit fetch total: ${cnt_all} ok: ${cnt_ok} unique: ${cnt_unq}\n` );
   return true;
 }
 
-async function poll( fn ) 
+export default class RedditInput
 {
-  if( !reddit.client )
-    return false;
-    
-  const t_now   = Date.now();
-  const t_prev  = _last_poll_time;
-
-  if( (t_now - t_prev) > _poll_interval ) 
+  get length(){ return CACHE.length; }
+  
+  static init()
   {
-    await fetch();
-    _last_poll_time = t_now;
+    reddit.init();
+    return !!reddit.client;
   }
   
-  if( _cache.length > 0 && fn )
+  static async poll( fn ) 
   {
-    fn( _cache[_cache.length - 1] );
-    _cache.pop();
-    return true;
-  }
-  return false;
-}
+    if( !reddit.client )
+      return false;
+      
+    const t_now   = Date.now();
+    const t_prev  = LAST_POLL_TIME;
 
-export default {
-  init,
-  poll,
-  get length(){ return _cache.length; }
-};
+    if( (t_now - t_prev) > POLL_INTERVAL ) 
+    {
+      await fetch();
+      LAST_POLL_TIME = t_now;
+    }
+    
+    if( CACHE.length > 0 && fn )
+    {
+      fn( CACHE[CACHE.length - 1] );
+      CACHE.pop();
+      return true;
+    }
+    return false;
+  }
+
+}
